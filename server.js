1b669eaae57f47410de4ba12f57737dde08092d2
bryxnt1 <bryantperez@bryants-MacBook-Pro.local>
2022-12-22 15:46:54 -0500

    update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ db.on("connected", () => {
   console.log(chalk.blue("Connected to MongoDB!"));
   app.listen(PORT, () => {
     console.log(
-      `Express server is running in development on port :${PORT}`
+      `Express server is running in development on port: ${PORT}`
     );
   });
 });
